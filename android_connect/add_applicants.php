65cd178425449f2a4899ae90e83f700acf68dd8a
jigar1111 <jigarsoni1111@gmail.com>
2016-09-03 01:06:05 +0530

    php_scripts

---
+++ android_connect/add_applicants.php
@@ -0,0 +1,31 @@
+
+<?php
+ 
+/*
+ * Following code will get single product details
+ * A product is identified by product id (pid)
+ */
+ 
+// array for JSON response
+$response = array();
+ 
+// include db connect class
+require_once __DIR__ . '/db_config.php';
+ 
+// connecting to db
+//$db = new DB_CONNECT();
+$con = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE); 
+
+$pid = 15;
+$sid = 1; 
+
+$result = mysqli_query($con,"INSERT INTO `drive_applications` (`app_id`, `drive_id`, `student_id`) VALUES (NULL, '$pid', '$sid')");
+// check for post data
+
+if($result) {
+	$response["success"] = 1;
+	$response["message"] = "Created";
+
+	echo json_encode($result);
+}
+?>
